13108c7ee43504d31c48a439261968d1d8d20089
Austin Kapin <kenaniaustin2@gmail.com>
2025-08-17 14:06:47 +0300

    Add collected static files for Render deployment

--- infinity_links/settings.py
+++ infinity_links/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # ======================
 STATIC_URL = '/static/'
 
-# Make sure this folder exists on disk:
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -127,11 +126,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media - Cloudinary
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
 MEDIA_URL = '/media/'
 
 # ======================
